96f8b934e9af6f353f691cb5f4854f4e609ec980
kawashima1129 <k.ktak1129@gmail.com>
2017-11-21 13:08:11 +0900

    [add]セレクトリストに初期値を付与

--- UnitCalc/calc/forms.py
+++ UnitCalc/calc/forms.py
@@ -6,12 +6,17 @@ class TimeTableForm(forms.Form):
     unit = forms.ModelChoiceField(
         label='',
         widget=forms.Select,
-        queryset = Unit.objects.all()
-
+        queryset = Unit.objects.all(),
+        error_messages={
+            'required': "You didn't select a choice.",
+            'invalid_choice': "invalid choice.",
+        },
+        initial=Unit.objects.get(subject_name='なし')
     )
 
     def calc(self):
         pass
+        #assert(self.is_valid())
 
 class MyForm(forms.Form):
 	text = forms.CharField(max_length=100, required=False, label='テキスト')
